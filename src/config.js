38c3dcb8ea5c6f6285e673be8460110101e912ae
monia <moniatec@gmail.com>
2020-05-15 19:55:17 -0400

    more updates to get heroku deployed

--- src/config.js
+++ src/config.js
@@ -1,1 +1,4 @@
-export const apiBaseUrl = process.env.REACT_APP_API_BASE_URL;
+const production = process.env.NODE_ENV === "production";
+export const apiBaseUrl = production
+  ? "https://shoppy-art-backend.herokuapp.com"
+  : process.env.REACT_APP_API_BASE_URL;
